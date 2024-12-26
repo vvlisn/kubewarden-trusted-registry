be1c23070c2a56fd98d7795dd487417b4d22861a
vvhuang-ll <13680846391@163.com>
2024-12-26 11:22:47 +0800

    delete Comment

--- validate.go
+++ validate.go
@@ -14,7 +14,7 @@ import (
 const httpBadRequestStatusCode = 400
 
 func validate(payload []byte) ([]byte, error) {
-	// 解析 ValidationRequest
+
 	validationRequest, err := parseValidationRequest(payload)
 	if err != nil {
 		return kubewarden.RejectRequest(
@@ -22,7 +22,6 @@ func validate(payload []byte) ([]byte, error) {
 			kubewarden.Code(httpBadRequestStatusCode))
 	}
 
-	// 从 ValidationRequest 获取配置
 	settings, err := NewSettingsFromValidationReq(validationRequest)
 	if err != nil {
 		return kubewarden.RejectRequest(
@@ -30,45 +29,42 @@ func validate(payload []byte) ([]byte, error) {
 			kubewarden.Code(httpBadRequestStatusCode))
 	}
 
-	// 解析 Pod 对象
 	pod, err := parsePod(validationRequest.Request.Object)
 	if err != nil {
 		return kubewarden.RejectRequest(
 			kubewarden.Message(err.Error()),
 			kubewarden.Code(httpBadRequestStatusCode))
 	}
-	// 将 []*corev1.Container 转换为 []corev1.Container
+
 	var containers []corev1.Container
 	if pod.Spec.Containers != nil {
 		for _, container := range pod.Spec.Containers {
 			containers = append(containers, *container)
 		}
 	}
-	// 验证 Pod 中的容器镜像
+
 	if err := validateContainers(containers, settings.TrustedRegistries); err != nil {
 		return kubewarden.RejectRequest(
 			kubewarden.Message(err.Error()),
 			kubewarden.NoCode)
 	}
-	// 将 []*corev1.Container 转换为 []corev1.Container
+
 	var initContainers []corev1.Container
 	if pod.Spec.InitContainers != nil {
 		for _, initContainer := range pod.Spec.InitContainers {
 			initContainers = append(initContainers, *initContainer)
 		}
 	}
-	// 验证 Pod 中的初始化容器镜像
+
 	if err := validateContainers(initContainers, settings.TrustedRegistries); err != nil {
 		return kubewarden.RejectRequest(
 			kubewarden.Message(err.Error()),
 			kubewarden.NoCode)
 	}
 
-	// 所有验证通过，接受请求
 	return kubewarden.AcceptRequest()
 }
 
-// 解析验证请求
 func parseValidationRequest(payload []byte) (*kubewarden_protocol.ValidationRequest, error) {
 	validationRequest := &kubewarden_protocol.ValidationRequest{}
 	if err := json.Unmarshal(payload, validationRequest); err != nil {
@@ -78,7 +74,6 @@ func parseValidationRequest(payload []byte) (*kubewarden_protocol.ValidationRequ
 	return validationRequest, nil
 }
 
-// 解析 Pod 对象
 func parsePod(podJSON json.RawMessage) (*corev1.Pod, error) {
 	pod := &corev1.Pod{}
 	if err := json.Unmarshal([]byte(podJSON), pod); err != nil {
@@ -88,7 +83,6 @@ func parsePod(podJSON json.RawMessage) (*corev1.Pod, error) {
 	return pod, nil
 }
 
-// 验证容器镜像是否来自受信任的仓库
 func validateContainers(containers []corev1.Container, trustedRegistries mapset.Set[string]) error {
 	for _, container := range containers {
 		logger.Debug(fmt.Sprintf("Checking container image: %s", container.Image))
@@ -101,7 +95,6 @@ func validateContainers(containers []corev1.Container, trustedRegistries mapset.
 	return nil
 }
 
-// 判断镜像是否来自受信任的仓库
 func isImageTrusted(image string, trustedRegistries mapset.Set[string]) bool {
 	for _, registry := range trustedRegistries.ToSlice() {
 		if strings.HasPrefix(image, registry) {
